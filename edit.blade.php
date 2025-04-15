61e848d00dc12b32c8a2f46234457ab0596a6863
salmasalsanabila <salma.salsa2008@smkn4-tsm.sch.id>
2025-04-15 21:18:17 +0700

    Add files via upload

--- edit.blade.php
+++ edit.blade.php
@@ -3,66 +3,95 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Akun</title>
-    <style type="text/css">
-        table {
-            width: 100%;
-            border-collapse:collapse;
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit Data</title>
+    <style>
+        table{
+            width:100%;
+            border-collapse: collapse;
             margin: 20px 0px;
         }
 
         table,
         th,
-        td {
+        td{
             border: 1px solid;
         }
-        </style>
+    </style>
 </head>
 <body>
-    <h1>Edit Akun<h1>
-        <a href="{{ route('akun.index') }}">Kembali</a><br><br>
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-    </ul>
- </div>
- @endif
- 
- <form action="{{ route('akun.update',$akun->id ) }}"method="POST" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
-    <h2>Data Akun</h2>
-    <label>Hak Akses</label><br>
-    <input type="text" id="name" name="name" value="{{ $akun->name }}">
+    <h1>Edit Siswa</h1>
+    <a href="{{ route('siswa.index') }}">Kembali</a>
     <br><br>
-    <label>Hak Akses</label><br>
-    <select name="usertype" required>
-        <option {{ 'admin' == $akun->usertype ? 'selected' : ''}} value="admin">Admin</option>
-        <option {{ 'ptk' == $akun->usertype ? 'selected' : ''}} value="ptk">PTK</option>
-    </select>
-    <br><br>
-    <button type="submit"> SIMPAN DATA</button>
-    <br><br>
-    </form>
-    <form action="{{ route('updateEmail',$akun->id) }}" method="POST" enctype="multipart/form-data">
+
+    @if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form action="{{ route('siswa.update', $siswa->id)}}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
-        <label>Password</label><br>
-        <input type="password" id="password" name="password">
+        <h2>Data Siswa</h2>
+        <label>Foto Siswa</label>
+        <img src="{{ asset('storage/siswas/' .$siswa->image) }}" width="120px" height="120px">
+        <input type="file" name="image" accept="image/*">
         <br><br>
 
-        <label for="password_confirmation" class="col-md-4 col-form-label text-md-end text-start">Confirm Passworrd</label>
-        <div class="col-md-6">
-            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
-    </div>
-    <br><br>
-    <button type="submit">SIMPAN PASSWORD</button>
-    <br><br>
+        <label >Nis Siswa</label>
+        <input type="text" name="nis" id="nis" value="{{ old('nis', $siswa->nis) }}" required>
+        <br><br>
+
+        <label>Nama Lengkap</label>
+        <input type="text" name="name" id="name" value="{{ old('name', $siswa->name) }}" required>
+        <br><br>
+
+        <label>Tingkatan</label>
+        <select name="tingkatan">
+            <option {{'X' == $siswa->tingkatan ? 'selected' : ''}} value="X"> X </option>
+            <option {{'XI' == $siswa->tingkatan ? 'selected' : ''}} value="XI"> XI </option>
+            <option {{'XII' == $siswa->tingkatan ? 'selected' : ''}} value="XII"> XII </option>
+        </select>
+        <br><br>
+
+        <label>Jurusan</label>
+        <select name="jurusan">
+            <option {{'TBSM' == $siswa->jurusan ? 'selected' : ''}} value="TBSM"> TBSM </option>
+            <option {{'TJKT' == $siswa->jurusan ? 'selected' : ''}} value="TJKT"> TJKT </option>
+            <option {{'PPLG' == $siswa->jurusan ? 'selected' : ''}} value="PPLG"> PPLG </option>
+            <option {{'DKV' == $siswa->jurusan ? 'selected' : ''}} value="DKV"> DKV </option>
+            <option {{'TOI' == $siswa->jurusan ? 'selected' : ''}} value="TOI"> TOI </option>
+        </select>
+        <br><br>
+
+        <label>Kelas</label>
+        <select name="kelas">
+            <option {{'1' == $siswa->kelas ? 'selected' : ''}} value="1"> 1 </option>
+            <option {{'2' == $siswa->kelas ? 'selected' : ''}} value="2"> 2 </option>
+            <option {{'3' == $siswa->kelas ? 'selected' : ''}} value="3"> 3 </option>
+            <option {{'4' == $siswa->kelas ? 'selected' : ''}} value="4"> 4 </option>
+        </select>
+        <br><br>
+
+        <label>No Hp</label>
+        <input type="text" name="hp" value="{{ old('hp', $siswa->hp) }}" required>
+        <br><br>
+
+        <label>Status</label>
+        <select name="status">
+            <option {{'1' == $siswa->status ? 'selected' : ''}} value="1"> Aktif </option>
+            <option {{'0' == $siswa->status ? 'selected' : ''}} value="0"> Tidak Aktif </option>
+        </select>
+        <br>
+        <br>
+
+        <button type="submit">SIMPAN DATA</button>
+        <button type="reset">RESET DATA</button>
     </form>
-
-
 </body>
 </html>
--- index.blade.php
+++ index.blade.php
@@ -3,79 +3,79 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data User</title>
+    <title>Data Siswa</title>
 </head>
 <body>
-    <h1>Data User</h1>
-    <a href="{{ route('admin.dashboard') }}">Menu Utama</a> <br>
+    <h1>Data Siswa</h1>
+    <a href="{{ route('admin.dashboard') }}">Menu Utama</a>
+    <br>
     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-    <br><br>
-
-    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+    <br>
+    <form action="{{ route('logout') }}" id="logout-form" method="POST">
         @csrf
     </form>
-
-    <br><br>
-
-    <!-- Form pencarian -->
-    <form action="" method="GET">
+    <br>
+    <form action="" method="get">
         <label>Cari :</label>
-        <input type="text" name="cari" value="{{ request()->get('cari') }}">
+        <input type="text" name="cari">
         <input type="submit" value="Cari">
     </form>
-
-    <br><br>
-    <a href="{{ route('akun.create') }}">Tambah User</a>
+    <br>
+    <a href="{{ route('siswa.create') }}">Tambah Siswa</a>
 
     @if(Session::has('success'))
-        <div class="alert alert-success" role="alert">
-            {{ Session::get('success') }}
-        </div>
+    <div class="alert-alert-success" role="alert">
+        {{ Session::get('success') }}
+    </div>
     @endif
-<!--Tabel Data User-->
-<table border="1" cellpadding="10"cellspacing="0">
-            <tr>
-                <th>Nama</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($users as $user)
-                <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->usertype }}</td>
-                    <td>
-                        <!-- Tombol Edit -->
-                        <a href="{{ route('akun.edit', $user->id) }}" class="btn btn-sm btn-primary">Edit</a>
 
-                        <!-- Tombol Hapus -->
-                        @if ($user->usertype == 'Siswa')
-                            <form onsubmit="return confirm('Jika Akun Siswa dihapus, maka Data Siswa akan terhapus. Apakah Anda yakin?');" action="{{ route('akun.destroy', $user->id) }}" method="POST" style="display: inline;">
-                        @else
-                            <form onsubmit="return confirm('Apakah Anda yakin ingin menghapus akun ini?');" action="{{ route('akun.destroy', $user->id) }}" method="POST" style="display: inline;">
-                        @endif
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
-                            </form>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="4">Data tidak ditemukan</td>
-                </tr>
-            @endforelse
-        </tbody>
+
+    <table class="tabel">
+        <tr>
+            <th>Foto</th>
+            <th>NIS</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Kelas</th>
+            <th>No Hp</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+        @forelse($siswas as $siswa)
+        <tr>
+            <td>
+                <img src="{{ asset('storage/public/siswas/' . $siswa->image) }}" width="120px" height="120px">
+            </td>
+            <td>{{ $siswa->nis }}</td>
+            <td>{{ $siswa->name }}</td>
+            <td>{{ $siswa->email }}</td>
+            <td>{{ $siswa->tingkatan }} {{ $siswa->jurusan }} {{ $siswa->kelas}}</td>
+            @if ($siswa->status==1)
+            <td>Aktif</td>
+            @else
+            <td>Tidak Aktif</td>
+            @endif
+            <td>
+                <form action="{{ route('siswa.destroy',$siswa->id) }}" onsubmit="return confirm('Anda Yakin ?')" method="POST">
+                    <a href="{{ route('siswa.show', $siswa->id) }} " class="btn btn-sm btn-dark">SHOW</a>
+                    <a href="{{ route('siswa.edit', $siswa->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">HAPUS</button>
+                </form>
+            </td>
+        </tr>
+        @empty
+        <tr>
+            <td>
+                <p>Data Tidak Ditemukan</p>
+            </td>
+            <td>
+                <a href="{{ route('siswa.index') }}">Kembali</a>
+            </td>
+        </tr>
+        @endforelse
     </table>
-
-    <br>
-
-    <!-- Pagination -->
-    {{ $users->links() }}
-    
+    {{ $siswas->links() }}
 </body>
 </html>
